6ea1d1a0c018863dc4551de615637b7e53a8d760
B00161001 Daminabo Tom-George <B00161001@mytudublin.ie>
2025-10-22 10:07:22 +0100

    Completed step 1

---
+++ processNewVisit.php
@@ -0,0 +1,22 @@
+<?php
+include('dbcon.php');
+
+$visitDate = $_POST['visitDate'];
+$visitTime = $_POST['visitTime'];
+$doctorID  = $_POST['docid'];
+$patientID = $_POST['ptntid'];
+$cost      = $_POST['cost'];
+
+$sql = "INSERT INTO visit (visit_date, visit_time, doctor_id, patient_id, cost)
+        VALUES ('$visitDate', '$visitTime', '$doctorID', '$patientID', '$cost');";
+
+echo "<pre>$sql</pre>";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New visit record inserted successfully!";
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
